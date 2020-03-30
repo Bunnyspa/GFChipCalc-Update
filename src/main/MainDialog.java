71ef04ad972aeaf88908326e7079a345ea25b479
Bunnyspa <bunnyspa242@gmail.com>
2020-03-30 07:23:06 -0500

    Update MainDialog.java

--- src/main/MainDialog.java
+++ src/main/MainDialog.java
@@ -16,9 +16,7 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JDialog;
 import javax.swing.JOptionPane;
-import javax.swing.SwingUtilities;
 
 /**
  *
